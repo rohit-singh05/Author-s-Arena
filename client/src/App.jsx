7c57794efaf2dfd9abc62d352225c02c84ba40f7
ELBERLIN <banditrohit05@gmail.com>
2024-03-17 15:10:26 +0530

    Template Created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Projects from './pages/Projects'
+import Dashboard from './pages/Dashboard'
 
 function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/projects' element={<Projects />} />
+
+      </Routes>
+    </BrowserRouter>
   )
 }
 
