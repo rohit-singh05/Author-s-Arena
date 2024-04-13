b96f771997854087e0cec4825c08c7ef6502bc6c
puskar789 <pbohara231159@gmail.com>
2024-04-14 02:04:11 +0530

    added like,edit,delete comment api route

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,7 +1,10 @@
 import express from "express";
 import {
   createComment,
+  deleteComment,
+  editComment,
   getPostComments,
+  likeComment,
 } from "../controllers/comment.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -9,5 +12,8 @@ const router = express.Router();
 
 router.post("/create", verifyToken, createComment);
 router.get("/getPostComments/:postId", getPostComments);
+router.put("/likeComment/:commentId", verifyToken, likeComment);
+router.put("/editComment/:commentId", verifyToken, editComment);
+router.delete("/deleteComment/:commentId", verifyToken, deleteComment);
 
 export default router;
