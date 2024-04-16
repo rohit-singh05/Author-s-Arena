71a3f1c13f4b22cf8d03e2939ff2a0d8e9c909e0
Schaaz <113603403+Schaaz@users.noreply.github.com>
2024-04-16 21:19:40 +0530

    Included Button

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,4 +1,4 @@
-import { TextInput } from 'flowbite-react'
+import { TextInput, Button } from 'flowbite-react'
 import React, { useEffect, useRef, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage'
