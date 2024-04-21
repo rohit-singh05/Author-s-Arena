d1e1f9e4a744a0b6f2a5b9e11c51b318729a9e05
ELBERLIN <banditrohit05@gmail.com>
2024-04-21 15:36:52 +0530

    Added signout in header also

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -2,10 +2,13 @@ import React, { useState, useEffect } from 'react'
 import { Sidebar } from 'flowbite-react'
 import { HiArrowSmRight, HiUser } from 'react-icons/hi'
 import { useLocation, Link } from 'react-router-dom'
+import { signoutSuccess } from '../redux/user/userSlice';
+import { useDispatch } from 'react-redux';
 
 function DashSidebar() {
     const location = useLocation()
     const [tab, setTab] = useState('')
+    const dispatch = useDispatch();
 
     useEffect(() => {
         const urlParams = new URLSearchParams(location.search)
@@ -15,6 +18,23 @@ function DashSidebar() {
             setTab(tabFromUrl);
         }
     }, [location.search])
+
+    const handleSignout = async () => {
+        try {
+            const res = await fetch("/api/user/signout", {
+                method: "POST",
+            });
+            const data = await res.json();
+            if (!res.ok) {
+                console.log(data.message);
+            } else {
+                dispatch(signoutSuccess());
+            }
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
+
     return (
         <Sidebar className='w-full md:w-56'>
             <Sidebar.Items>
@@ -24,7 +44,7 @@ function DashSidebar() {
                             Profile
                         </Sidebar.Item>
                     </Link>
-                    <Sidebar.Item icon={HiArrowSmRight} className='cursor-pointer'>
+                    <Sidebar.Item icon={HiArrowSmRight} className='cursor-pointer' onClick={handleSignout}>
                         Sign Out
                     </Sidebar.Item>
                 </Sidebar.ItemGroup>
