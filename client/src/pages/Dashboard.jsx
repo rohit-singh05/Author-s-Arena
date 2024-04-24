5dd576f14634ede99ff81b147f48df09bf669da7
Schaaz <schaaz3allme@gmail.com>
2024-04-24 18:49:13 +0530

    Added posts section to admin dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import { useLocation } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import DashSidebar from '../components/DashSidebar'
 import DashProfile from '../components/DashProfile'
+import DashPosts from '../components/DashPosts'
 
 function Dashboard() {
   const location = useLocation()
@@ -23,6 +24,8 @@ function Dashboard() {
         <DashSidebar />
       </div>
       {tab === 'profile' && <DashProfile />}
+      {/*Posts*/}
+      {tab === 'posts' && <DashPosts />}
     </div>
   )
 }
