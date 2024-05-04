6932b94d902832dc9d1335df52c852d1df5b77c8
Schaaz <schaaz3allme@gmail.com>
2024-05-04 21:52:05 +0530

    Post, Comments, Recent Articles

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -53,7 +53,7 @@ export const likeComment = async (req, res, next) => {
       comment.likes.push(req.user.id);
     } else {
       comment.numberOfLikes -= 1;
-      comment.likes.slice(userIndex, 1);
+      comment.likes.splice(userIndex, 1);
     }
     await comment.save();
     res.status(200).json(comment);
