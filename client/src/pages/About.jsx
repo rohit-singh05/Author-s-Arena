566883893f753133d00d9a0c3a8f3a66f15a1b0e
ELBERLIN <banditrohit05@gmail.com>
2024-06-08 11:45:42 +0530

    Made changes to the name

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -8,10 +8,8 @@ export default function About() {
           </h1>
           <div className='text-md text-gray-500 flex flex-col gap-6'>
             <p>
-              Welcome to Sahand's Blog! This blog was created by Sahand Ghavidel
-              as a personal project to share his thoughts and ideas with the
-              world. Sahand is a passionate developer who loves to write about
-              technology, coding, and everything in between.
+              Welcome to Writer's Den! This blog was created as a personal project to share his thoughts and ideas with the
+              world.
             </p>
 
             <p>
